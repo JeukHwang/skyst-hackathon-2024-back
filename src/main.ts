2db8fef31b4dcb99ad6271d9f9c2dfaa9a245741
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2024-03-23 14:51:21 +0900

    feat: configure cookieParser, cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 import { config } from 'dotenv';
 import { AppModule } from './app.module';
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.enableCors({
+    origin: [
+      'http://localhost:5173', // for frontend in development
+      'https://molato.netlify.app/', // for frontend in production
+    ],
+    credentials: true,
+  });
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT);
   if (process.env.NODE_ENV !== 'production') {
