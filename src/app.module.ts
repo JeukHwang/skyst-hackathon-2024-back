4f2584e8ad9b054fb86ac67033e548b98de7844e
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2024-03-23 14:47:54 +0900

    feat: init item

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { TestModule } from './test/test.module';
+import { ItemModule } from './item/item.module';
 
 @Module({
-  imports: [PrismaModule, TestModule],
+  imports: [PrismaModule, TestModule, ItemModule],
   controllers: [AppController],
   providers: [AppService],
 })
