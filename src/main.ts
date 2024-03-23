594a9ac40a0badbad757f158af904cb0d88f7e7e
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2024-03-23 13:26:21 +0900

    feat: configure custom setting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { config } from 'dotenv';
 import { AppModule } from './app.module';
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+  if (process.env.NODE_ENV !== 'production') {
+    console.log(`Server running on http://localhost:${PORT}`);
+  }
 }
 bootstrap();
