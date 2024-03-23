6a2b75d7eb74a32963a7bd81d6d6990d2272d23d
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2024-03-23 17:39:24 +0900

    feat: implement gift, item apis

--- src/gift/gift.controller.ts
+++ src/gift/gift.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { CurrentUser } from 'src/user/user.decorator';
+import { ReplyRequestDto, SendRequestDto } from './gift.dto';
+import { GiftProfile, GiftService } from './gift.service';
 
 @Controller('gift')
-export class GiftController {}
+export class GiftController {
+  constructor(private readonly giftService: GiftService) {}
+
+  @Get('sended')
+  async getSendedGift(@CurrentUser() user: User): Promise<GiftProfile[]> {
+    return await this.giftService.getSendedGift(user.id);
+  }
+
+  @Get('received')
+  async getReceivedGift(@CurrentUser() user: User): Promise<GiftProfile[]> {
+    return await this.giftService.getReceivedGift(user.id);
+  }
+
+  @Post('send')
+  async sendGift(
+    @Body() body: SendRequestDto,
+    @CurrentUser() user: User,
+  ): Promise<GiftProfile> {
+    return await this.giftService.sendGift(body, user);
+  }
+
+  @Post('reply')
+  async replyGift(
+    @Body() body: ReplyRequestDto,
+    @CurrentUser() user: User,
+  ): Promise<void> {
+    return await this.giftService.replyGift(body, user);
+  }
+
+  @Get(':id')
+  find(@Param('id') giftId: string, user: User): Promise<GiftProfile> {
+    return this.giftService.find(giftId, user);
+  }
+}
--- src/gift/gift.service.ts
+++ src/gift/gift.service.ts
@@ -1,4 +1,105 @@
 import { Injectable } from '@nestjs/common';
+import { Gift, User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ReplyRequestDto, SendRequestDto } from './gift.dto';
+
+export type GiftProfile = {
+  id: string;
+  letter: string;
+  repliedLetter?: string;
+  itemId: string;
+  senderId: string;
+  receiverId: string;
+  receivedAt?: Date;
+  replySendedAt?: Date;
+  replyReceivedAt?: Date;
+  deletedAt?: Date;
+};
+
+export function toGiftProfile(gift: Gift): GiftProfile {
+  return {
+    id: gift.id,
+    letter: gift.letter,
+    repliedLetter: gift.repliedLetter,
+    itemId: gift.itemId,
+    senderId: gift.senderId,
+    receiverId: gift.receiverId,
+    receivedAt: gift.receivedAt,
+    replySendedAt: gift.replySendedAt,
+    replyReceivedAt: gift.replyReceivedAt,
+    deletedAt: gift.replySendedAt,
+  };
+}
 
 @Injectable()
-export class GiftService {}
+export class GiftService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getSendedGift(userId: string): Promise<GiftProfile[]> {
+    const gifts = await this.prismaService.user
+      .findUnique({ where: { id: userId } })
+      .sended();
+    return gifts.map(toGiftProfile);
+  }
+
+  async getReceivedGift(userId: string): Promise<GiftProfile[]> {
+    const gifts = await this.prismaService.user
+      .findUnique({ where: { id: userId } })
+      .received();
+    return gifts.map(toGiftProfile);
+  }
+
+  async sendGift(body: SendRequestDto, user: User): Promise<GiftProfile> {
+    const gift = await this.prismaService.gift.create({
+      data: {
+        letter: body.letter,
+        itemId: body.itemId,
+        senderId: user.id,
+        receiverId: body.receiverId,
+        sendedAt: new Date(),
+      },
+    });
+    return toGiftProfile(gift);
+  }
+
+  async replyGift(body: ReplyRequestDto, user: User): Promise<void> {
+    const gift = await this.prismaService.gift.findUnique({
+      where: { id: body.giftId },
+    });
+    if (
+      !gift &&
+      gift.receiverId !== user.id &&
+      gift.repliedLetter === undefined
+    )
+      return null;
+    await this.prismaService.gift.update({
+      where: { id: body.giftId },
+      data: {
+        repliedLetter: body.repliedLetter,
+        replySendedAt: new Date(),
+      },
+    });
+  }
+
+  async find(giftId: string, user: User): Promise<GiftProfile> {
+    const gift = await this.prismaService.gift.findUnique({
+      where: { id: giftId },
+    });
+    await this.prismaService.gift.update({
+      where: { id: gift.id },
+      data: {
+        receivedAt:
+          user.id === gift.receiverId && gift.sendedAt && !gift.receivedAt
+            ? new Date()
+            : undefined,
+        replyReceivedAt:
+          user.id === gift.senderId &&
+          gift.replySendedAt &&
+          !gift.replyReceivedAt
+            ? new Date()
+            : undefined,
+      },
+    });
+    return toGiftProfile(gift);
+  }
+}
